f136a34eb49a675042540b4dec3e7be4ae1e5771
Krzysztof Maziarz <Krzysztof.Maziarz@microsoft.com>
2023-12-18 10:59:12 +0000

    Rename top-level package to chemformer and add dependencies

--- setup.py
+++ setup.py
@@ -1,7 +1,17 @@
 from setuptools import setup
 
 setup(
-    name="molbart",
-    version="0.1.0",
-    packages=["molbart", "molbart.models", "molbart.data"]
+    name="syntheseus-chemformer",
+    version="0.1.1",
+    description="Fork of Chemformer for use in the syntheseus library",
+    package_dir={
+        "chemformer": ".",
+        "chemformer.molbart": "molbart"
+    },
+    package_data={"": ["*.txt"]},
+    install_requires=[
+        "pytorch-lightning==1.9.4",
+        "syntheseus-PySMILESutils"
+    ],
+    url="https://github.com/kmaziarz/Chemformer",
 )
