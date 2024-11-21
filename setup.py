15f8f4cbe617e3884239446b3171a16c1370ac81
Krzysztof Maziarz <Krzysztof.Maziarz@microsoft.com>
2024-11-21 14:04:11 +0000

    Pin torchmetrics to 1.5.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="syntheseus-chemformer",
-    version="0.1.1",
+    version="0.2.0",
     description="Fork of Chemformer for use in the syntheseus library",
     package_dir={
         "chemformer": ".",
@@ -11,6 +11,7 @@ setup(
     package_data={"": ["*.txt"]},
     install_requires=[
         "pytorch-lightning==1.9.4",
+        "torchmetrics==1.5.1",
         "syntheseus-PySMILESutils"
     ],
     url="https://github.com/kmaziarz/Chemformer",
